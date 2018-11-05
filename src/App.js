51b05f382a3cabbad166fe2184883aa19b1ce116
Yohan Atlan <yohan.atlan@hetic.net>
2018-11-05 18:04:14 +0200

    add list of movies and series

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state: State = {
+      movies: [],
+      series: []
+  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: [],
+      series: []
+    }
+  }
+  getMoovies() {
+    fetch('https://api.themoviedb.org/3/movie/popular?api_key=92b418e837b833be308bbfb1fb2aca1e&language=en-US&page=1')
+    .then(results => {
+        return results.json()
+    })
+    .then(results => {
+      this.setState({movies: results.results})
+    })
+  }
+  getSeries() {
+    fetch('https://api.themoviedb.org/3/tv/popular?api_key=92b418e837b833be308bbfb1fb2aca1e&language=fr-FR&page=1')
+    .then(results => {
+        return results.json()
+    })
+    .then(results => {
+      this.setState({series: results.results})
+    })
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <button onClick={() => this.getMoovies()}>
+            Fetch movies
+          </button>
+          <button onClick={() => this.getSeries()}>
+            Fetch series
+          </button>
+          <ul>
+            {this.state.movies.map(function(movie, index){
+              return <h1 key={index}>{movie.title}</h1>
+            })}
+          </ul>
+          <ul>
+            {this.state.series.map(function(serie, index){
+              return <h1 key={index}>{serie.name}</h1>
+            })}
+          </ul>
         </header>
       </div>
     );
